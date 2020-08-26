5f1f6dcee8e6febf4daa5fed694ca8f78f757f7f
piyush46749 <piyush46749@gmail.com>
2020-08-26 00:48:32 -0300

    extracting extension of all the files

--- organize.py
+++ organize.py
@@ -3,3 +3,13 @@
 from pathlib import Path
 from sys import argv
 from shutil import move
+
+def get_dir(filename):
+    """
+        This function takes filename and returns name of the
+        parent directory of the respective filename
+        Returns Miscellaneous if Parent is not found
+    """
+    ext = filename.suffix[1:]
+    return dirs.get(ext, "Miscellaneous")
+
